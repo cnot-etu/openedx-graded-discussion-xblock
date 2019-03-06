160dbbbe0af8d410d062f9cc62aebcfd978f4b63
Andrey Canon <andrey.canon@edunext.co>
2019-03-06 12:16:52 -0500

    Create student list, filter section and contribution section

--- graded_discussion/static/js/src/graded_discussion.js
+++ graded_discussion/static/js/src/graded_discussion.js
@@ -2,13 +2,13 @@
 function GradedDiscussionXBlock(runtime, element) {
 
     function renderContributions(data){
-        $(".list-contributions").empty();
+        $(".list-contribution").empty();
 
         data.forEach(function(entry) {
             if ( entry.kind === "thread"){
-                $(".list-contributions").append(createThreadElement(entry.contribution, entry.created_at));
+                $(".list-contribution").append(createThreadElement(entry.contribution, entry.created_at));
             } else {
-                $(".list-contributions").append(createCommentElement(entry.contribution, entry.parent, entry.created_at));
+                $(".list-contribution").append(createCommentElement(entry.contribution, entry.parent, entry.created_at));
             }
         });
         updateDateTime();
@@ -46,25 +46,142 @@ function GradedDiscussionXBlock(runtime, element) {
     }
 
     function updateDateTime(){
-        $(".contributions-section .date-time").each(function() {
-            var date = new Date($(this).attr("value"));
-            $(this).text(date.toLocaleString());
+        $(".graded_discussion_block .date-time").each(function() {
+            var value = $(this).attr("value");
+            if (value==""){
+                $(this).text("This user has no contributions");
+            } else {
+                var date = new Date(value);
+                $(this).text(date.toLocaleString());
+            }
+
         });
     }
+
+    function sortByOldest(a, b){
+        var firstDate = $(a).find('span').attr("value");
+        var secondDate = $(b).find('span').attr("value");
+
+        if (firstDate == ""){
+            return 1;
+        } else if (secondDate == "") {
+            return -1;
+        }
+
+        return (firstDate > secondDate) ? 1 : -1;
+    }
+
+    function sortByNewest(a, b){
+        var firstDate = $(a).find('span').attr("value");
+        var secondDate = $(b).find('span').attr("value");
+
+        if (firstDate == ""){
+            return 1;
+        } else if (secondDate == "") {
+            return -1;
+        }
+
+        return (firstDate < secondDate) ? 1 : -1;
+    }
+
+    function getContributions(username){
+        $.ajax({
+            type: "GET",
+            url: reloadPage,
+            data: {"user": username},
+            success: renderContributions
+        });
+    }
+
 
     $(function ($) {
         /* Here's where you'd do things on page load. */
     });
 
     var reloadPage = runtime.handlerUrl(element, "refresh_data");
+    var enterGrade = runtime.handlerUrl(element, "enter_grade");
+
+    var grade = $("#grade");
+    var comment = $("#comment");
+    var allFields = $( [] ).add(grade).add(comment);
+
+    var dialog = $( "#dialog-form" ).dialog({
+      autoOpen: false,
+      height: 400,
+      width: 350,
+      modal: true,
+      buttons: {
+        "Submit": function(){
+            var score = $("#grade").val();
+            var comment = $("#comment").val();
+            var username = $(".graded_discussion_block .users-list .active").attr("username");
+            if (username == null ){
+                dialog.dialog("close");
+                alert("Select a user");
+            } else {
+                $.ajax({
+                    type: "GET",
+                    url: enterGrade,
+                    data: {"user": username, "score": score, "comment": comment},
+                });
+            }
+
+        },
+        Cancel: function() {
+          dialog.dialog("close");
+        }
+      },
+      close: function() {
+        allFields.removeClass("ui-state-error");
+      }
+    });
 
     updateDateTime();
 
-    $(".reload-button").click(function(){
-        $.ajax({
-            type: "GET",
-            url: reloadPage,
-            success: renderContributions
-        });
+    $(".student-section .reload-button").click(function(){
+        getContributions(null);
     })
+
+    $(".graded_discussion_block .oldest").click(function(){
+        $(".users-list li").sort(sortByOldest).appendTo('.users-list');
+    });
+
+    $(".graded_discussion_block .newest").click(function(){
+        $(".users-list li").sort(sortByNewest).appendTo('.users-list');
+    });
+
+    $(".graded_discussion_block .cohort-filter li").click(function(){
+        var id = $(this).attr("cohort-id");
+        $(".student-list-section .item").filter(function() {
+          $(this).toggle($(this).attr("cohort-id") === id)
+        });
+    });
+
+    $(".graded_discussion_block .teams-filter li").click(function(){
+        var id = $(this).attr("team-id");
+        $(".student-list-section .item").filter(function() {
+          $(this).toggle($(this).attr("team-id") === id)
+        });
+    });
+
+    $(".graded_discussion_block .users-list li").click(function(){
+        var username = $(this).attr("username");
+        $("li").removeClass("active");
+        $(this).addClass("active");
+        getContributions(username);
+    });
+
+    $(".graded_discussion_block #grade-button").click(function(){
+        dialog.dialog( "open" );
+    });
+
+    $(".graded_discussion_block .search-input").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $(".student-list-section .item").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+
+    $(".users-list li").sort(sortByOldest).appendTo('.users-list');
+
 }
